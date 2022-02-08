1d299086727678f3e3c7550be21278d8b07390f0
Mateo Estrada <mateoej12@gmail.com>
2022-02-07 21:03:23 -0800

    Added the predictor screen with a stateful widget.

--- lib/screens/predictor.dart
+++ lib/screens/predictor.dart
@@ -1,10 +1,47 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'dart:math';
 
-class Predictor extends StatelessWidget {
-  const Predictor({Key? key}) : super(key: key);
+List<String> listOfMessages = [
+  'Calling Next Monday',
+  'We Have Decided...',
+  'Hello?',
+  'Please take this coding text...',
+  'You have been choosen for...',
+  'We will email you!',
+  'Your application has been seen',
+  'You got the Job!',
+  'Congratulations!'
+];
+
+class ChangeText extends StatefulWidget {
+  const ChangeText({Key? key}) : super(key: key);
+
+  @override
+  _ChangeTextState createState() => _ChangeTextState();
+}
+
+class _ChangeTextState extends State<ChangeText> {
+  String textToShow = '';
+  void changeMessage() {
+    final _random = Random();
+    setState(() {
+      textToShow = listOfMessages[_random.nextInt(listOfMessages.length)];
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Text('Predictor');
+    return Column(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+      const Text('Will I get a call back?',
+          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30)),
+      GestureDetector(
+        onTap: () => changeMessage(),
+        child: const Text('(..Press..)',
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30)),
+      ),
+      Text(textToShow,
+          style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
+    ]);
   }
 }
